e6116fc9df4165f0be5baa3c73a90e905e6c7c9d
clayton-schneider <schneiderc2016@gmail.com>
2023-11-06 17:14:27 -0500

    sim now starts, and button control

--- src/Simulation.ts
+++ src/Simulation.ts
@@ -1,4 +1,5 @@
 import type Canvas from "./Canvas";
+import type Cursor from "./Cursor";
 
 class Simulation {
   bounces: number;
@@ -11,6 +12,55 @@ class Simulation {
 
   mountCanvas(entry: HTMLDivElement, canvas: Canvas) {
     entry.innerHTML = canvas.html;
+  }
+
+  toggleRunning() {
+    this.running = !this.running;
+  }
+
+  incrementBounce() {
+    this.bounces++;
+    document.querySelector(
+      "#bounces"
+    )!.innerHTML = `<p>Bounces: ${this.bounces.toString()} </p>`;
+  }
+
+  run(canvas: Canvas, cursor: Cursor) {
+    window.requestAnimationFrame(() => {
+      this.run(canvas, cursor);
+    });
+
+    this.update(canvas, cursor);
+    this.render(canvas, cursor);
+  }
+
+  update(canvas: Canvas, cursor: Cursor) {
+    if (!this.running) {
+      return;
+    }
+
+    if (cursor.getRightEdge() >= canvas.width) {
+      cursor.velX = -1 * cursor.velX;
+      this.incrementBounce();
+    }
+    if (cursor.getLeftEdge() <= 0) {
+      cursor.velX = -1 * cursor.velX;
+    }
+    if (cursor.getBottomEdge() >= canvas.height) {
+      cursor.velY = -1 * cursor.velY;
+    }
+    if (cursor.getTopEdge() <= 0) {
+      cursor.velY = -1 * cursor.velY;
+    }
+
+    cursor.x += cursor.velX;
+    cursor.y += cursor.velY;
+  }
+
+  render(canvas: Canvas, cursor: Cursor) {
+    canvas.getCtx().clearRect(0, 0, canvas.width, canvas.height);
+
+    canvas.getCtx().fillRect(cursor.x, cursor.y, cursor.width, cursor.height);
   }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -9,35 +9,8 @@ const cursor = new Cursor(100, 100, 10, 10, 160, 90);
 const sim = new Simulation();
 sim.mountCanvas(document.querySelector<HTMLDivElement>("#app")!, canvas);
 
-function main() {
-  window.requestAnimationFrame(main);
+sim.run(canvas, cursor);
 
-  update();
-  render();
-}
-
-main();
-
-function update() {
-  if (cursor.getRightEdge() >= canvas.width) {
-    cursor.velX = -1 * cursor.velX;
-  }
-  if (cursor.getLeftEdge() <= 0) {
-    cursor.velX = -1 * cursor.velX;
-  }
-  if (cursor.getBottomEdge() >= canvas.height) {
-    cursor.velY = -1 * cursor.velY;
-  }
-  if (cursor.getTopEdge() <= 0) {
-    cursor.velY = -1 * cursor.velY;
-  }
-
-  cursor.x += cursor.velX;
-  cursor.y += cursor.velY;
-}
-
-function render() {
-  canvas.getCtx().clearRect(0, 0, canvas.width, canvas.height);
-
-  canvas.getCtx().fillRect(cursor.x, cursor.y, cursor.width, cursor.height);
-}
+document.querySelector("#start")!.addEventListener("click", () => {
+  sim.toggleRunning();
+});
