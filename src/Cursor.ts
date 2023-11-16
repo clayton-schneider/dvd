b6c24c5ec6b96dacd8bab3870b074abf14e4e6b4
clayton-schneider <schneiderc2016@gmail.com>
2023-11-16 16:27:37 -0500

    big changes, drop in coding

--- src/Cursor.ts
+++ src/Cursor.ts
@@ -3,6 +3,8 @@ import Canvas from "./Canvas";
 class Cursor {
   startX: number;
   startY: number;
+  startVX: number;
+  startVY: number;
   x: number;
   y: number;
   velX: number;
@@ -20,6 +22,8 @@ class Cursor {
   ) {
     this.startX = x;
     this.startY = y;
+    this.startVX = velX;
+    this.startVY = velY;
     this.x = x;
     this.y = y;
     this.velX = velX;
@@ -45,141 +49,177 @@ class Cursor {
     // draw line to wall
     // change direction of v
 
+    const v = {
+      x: this.startVX,
+      y: this.startVY,
+    };
+
     const startCords = {
       x: this.startX,
       y: this.startY,
     };
+
+    if (v.x > 0) {
+      startCords.x += this.width;
+    }
+
+    if (v.y > 0) {
+      startCords.y += this.height;
+    }
 
     const coordinates = [];
     // can't pass directly in becuase then we would pass the reference
     // and since we update later those values would be overwritten
     coordinates.push({ x: startCords.x, y: startCords.y });
 
-    const v = {
-      x: this.velX,
-      y: this.velY,
-    };
+    for (let i = 0; i < 1; i++) {
+      let newCords = { x: 0, y: 0 };
 
-    for (let i = 0; i < 4; i++) {
-      if (v.x > 0 && v.y > 0) {
-        // moving bottom right
-        const horDist = canvas.getRightEdge() - startCords.x;
-        const vertDist = canvas.getBottomEdge() - startCords.y;
+      const horDist =
+        v.x > 0 ? canvas.getRightEdge() - startCords.x : startCords.x;
 
-        let newCords = { x: 0, y: 0 };
-        // what side is going to hit first
-        if (horDist > vertDist) {
-          // bottom will be hit
-          newCords.x = startCords.x + vertDist;
-          newCords.y = startCords.y + vertDist;
-          coordinates.push(newCords);
-          v.y = -1 * v.y;
-          startCords.x = newCords.x;
-          startCords.y = newCords.y;
-        } else if (horDist < vertDist) {
-          // right will be hit
-          newCords.y = startCords.y + horDist;
-          newCords.x = startCords.x + horDist;
-          coordinates.push(newCords);
-          v.x = -1 * v.x;
-          startCords.x = newCords.x;
-          startCords.y = newCords.y;
-        } else if (horDist === vertDist) {
-          // hits corner
-          console.log("slot!");
-        } else {
-          console.log("Error calculating distance");
-        }
-      } else if (v.x > 0 && v.y < 0) {
-        // moving top right
-        const horDist = canvas.getRightEdge() - startCords.x;
-        const vertDist = Math.abs(canvas.getTopEdge() - startCords.y);
+      const vertDist =
+        v.y > 0 ? canvas.getBottomEdge() - startCords.y : startCords.y;
 
-        let newCords = { x: 0, y: 0 };
-        // what side is going to hit first
-        if (horDist > vertDist) {
-          // top will be hit
-          newCords.x = startCords.x + vertDist;
-          newCords.y = startCords.y - vertDist;
-          coordinates.push(newCords);
-          v.y = -1 * v.y;
-          startCords.x = newCords.x;
-          startCords.y = newCords.y;
-        } else if (horDist < vertDist) {
-          // right will be hit
-          newCords.y = startCords.y - horDist;
-          newCords.x = startCords.x + horDist;
-          coordinates.push(newCords);
-          v.x = -1 * v.x;
-          startCords.x = newCords.x;
-          startCords.y = newCords.y;
-        } else if (horDist === vertDist) {
-          // hits corner
-          console.log("slot!");
-        } else {
-          console.log("Error calculating distance");
-        }
-      } else if (v.x < 0 && v.y > 0) {
-        // moving bottom left
-        const horDist = Math.abs(canvas.getLeftEdge() - startCords.x);
-        const vertDist = Math.abs(canvas.getBottomEdge() - startCords.y);
-
-        let newCords = { x: 0, y: 0 };
-        // what side is going to hit first
-        if (horDist > vertDist) {
-          // bottom will be hit
-          newCords.x = startCords.x - vertDist;
-          newCords.y = startCords.y + vertDist;
-          coordinates.push(newCords);
-          v.y = -1 * v.y;
-          startCords.x = newCords.x;
-          startCords.y = newCords.y;
-        } else if (horDist < vertDist) {
-          // left will be hit
-          newCords.y = startCords.y - horDist;
-          newCords.x = startCords.x - horDist;
-          coordinates.push(newCords);
-          v.x = -1 * v.x;
-          startCords.x = newCords.x;
-          startCords.y = newCords.y;
-        } else if (horDist === vertDist) {
-          // hits corner
-          console.log("slot!");
-        } else {
-          console.log("Error calculating distance");
-        }
-      } else if (v.x < 0 && v.y < 0) {
-        // moving top left
-        const horDist = Math.abs(canvas.getLeftEdge() - startCords.x);
-        const vertDist = Math.abs(canvas.getTopEdge() - startCords.y);
-
-        let newCords = { x: 0, y: 0 };
-        // what side is going to hit first
-        if (horDist > vertDist) {
-          // top will be hit
-          newCords.x = startCords.x - vertDist;
-          newCords.y = startCords.y - vertDist;
-          coordinates.push(newCords);
-          v.y = -1 * v.y;
-          startCords.x = newCords.x;
-          startCords.y = newCords.y;
-        } else if (horDist < vertDist) {
-          // right will be hit
-          newCords.y = startCords.y - horDist;
-          newCords.x = startCords.x - horDist;
-          coordinates.push(newCords);
-          v.x = -1 * v.x;
-          startCords.x = newCords.x;
-          startCords.y = newCords.y;
-        } else if (horDist === vertDist) {
-          // hits corner
-          console.log("slot!");
-        } else {
-          console.log("Error calculating distance");
-        }
-      } else {
-        console.log("Error reading v for path-gen");
+      console.log(horDist, vertDist);
+      // moving right & will hit top or bottom
+      if (v.x > 0 && horDist > vertDist) {
+        newCords.x = startCords.x + vertDist;
+        newCords.y = startCords.y + vertDist;
+        coordinates.push(newCords);
+        v.y = -1 * v.y;
       }
+
+      // if (horDist > vertDist) {
+      //   // will hit top or bottom
+      // } else if (horDist < vertDist) {
+      //   // will hit left or right
+      // } else if (horDist === vertDist) {
+      //   // will hit slot
+      //   console.log("slot!");
+      // } else {
+      //   console.log("ERROR comparing distances");
+      // }
+
+      // if (v.x > 0 && v.y > 0) {
+      //   // moving bottom right
+      //   const horDist = canvas.getRightEdge() - startCords.x;
+      //   const vertDist = canvas.getBottomEdge() - startCords.y;
+
+      //   let newCords = { x: 0, y: 0 };
+      //   // what side is going to hit first
+      //   if (horDist > vertDist) {
+      //     // bottom will be hit
+      //     newCords.x = startCords.x + vertDist;
+      //     newCords.y = startCords.y + vertDist;
+      //     coordinates.push(newCords);
+      //     v.y = -1 * v.y;
+      //     startCords.x = newCords.x;
+      //     startCords.y = newCords.y;
+      //   } else if (horDist < vertDist) {
+      //     // right will be hit
+      //     newCords.y = startCords.y + horDist;
+      //     newCords.x = startCords.x + horDist;
+      //     coordinates.push(newCords);
+      //     v.x = -1 * v.x;
+      //     startCords.x = newCords.x;
+      //     startCords.y = newCords.y;
+      //   } else if (horDist === vertDist) {
+      //     // hits corner
+      //     console.log("slot!");
+      //   } else {
+      //     console.log("Error calculating distance");
+      //   }
+      // } else if (v.x > 0 && v.y < 0) {
+      //   // moving top right
+      //   const horDist = canvas.getRightEdge() - startCords.x;
+      //   const vertDist = Math.abs(canvas.getTopEdge() - startCords.y);
+
+      //   let newCords = { x: 0, y: 0 };
+      //   // what side is going to hit first
+      //   if (horDist > vertDist) {
+      //     // top will be hit
+      //     newCords.x = startCords.x + vertDist;
+      //     newCords.y = startCords.y - vertDist;
+      //     coordinates.push(newCords);
+      //     v.y = -1 * v.y;
+      //     startCords.x = newCords.x;
+      //     startCords.y = newCords.y;
+      //   } else if (horDist < vertDist) {
+      //     // right will be hit
+      //     newCords.y = startCords.y - horDist;
+      //     newCords.x = startCords.x + horDist;
+      //     coordinates.push(newCords);
+      //     v.x = -1 * v.x;
+      //     startCords.x = newCords.x;
+      //     startCords.y = newCords.y;
+      //   } else if (horDist === vertDist) {
+      //     // hits corner
+      //     console.log("slot!");
+      //   } else {
+      //     console.log("Error calculating distance");
+      //   }
+      // } else if (v.x < 0 && v.y > 0) {
+      //   // moving bottom left
+      //   const horDist = Math.abs(canvas.getLeftEdge() - startCords.x);
+      //   const vertDist = Math.abs(canvas.getBottomEdge() - startCords.y);
+
+      //   let newCords = { x: 0, y: 0 };
+      //   // what side is going to hit first
+      //   if (horDist > vertDist) {
+      //     // bottom will be hit
+      //     newCords.x = startCords.x - vertDist;
+      //     newCords.y = startCords.y + vertDist;
+      //     coordinates.push(newCords);
+      //     v.y = -1 * v.y;
+      //     startCords.x = newCords.x;
+      //     startCords.y = newCords.y;
+      //   } else if (horDist < vertDist) {
+      //     // left will be hit
+      //     newCords.y = startCords.y - horDist;
+      //     newCords.x = startCords.x - horDist;
+      //     coordinates.push(newCords);
+      //     v.x = -1 * v.x;
+      //     startCords.x = newCords.x;
+      //     startCords.y = newCords.y;
+      //   } else if (horDist === vertDist) {
+      //     // hits corner
+      //     console.log("slot!");
+      //   } else {
+      //     console.log("Error calculating distance");
+      //   }
+      // } else if (v.x < 0 && v.y < 0) {
+      //   // moving top left
+      //   const horDist = Math.abs(canvas.getLeftEdge() - startCords.x);
+      //   const vertDist = Math.abs(canvas.getTopEdge() - startCords.y);
+
+      //   let newCords = { x: 0, y: 0 };
+      //   // what side is going to hit first
+      //   if (horDist > vertDist) {
+      //     // top will be hit
+      //     newCords.x = startCords.x - vertDist;
+      //     newCords.y = startCords.y - vertDist;
+      //     coordinates.push(newCords);
+      //     v.y = -1 * v.y;
+      //     startCords.x = newCords.x;
+      //     startCords.y = newCords.y;
+      //   } else if (horDist < vertDist) {
+      //     // right will be hit
+      //     newCords.y = startCords.y - horDist;
+      //     newCords.x = startCords.x - horDist;
+      //     coordinates.push(newCords);
+      //     v.x = -1 * v.x;
+      //     startCords.x = newCords.x;
+      //     startCords.y = newCords.y;
+      //   } else if (horDist === vertDist) {
+      //     // hits corner
+      //     console.log("slot!");
+      //   } else {
+      //     console.log("Error calculating distance");
+      //   }
+      // } else {
+      //   console.log("Error reading v for path-gen");
+      // }
     }
 
     return coordinates;
--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,353 @@
-import Cursor from "./Cursor";
+// import Cursor from "./Cursor";
+// import Simulation from "./Simulation";
+
 import Canvas from "./Canvas";
-import Simulation from "./Simulation";
 
-const CANVAS_WIDTH = 1600;
-const CANVAS_HEIGHT = 900;
+type Line = {
+  xStart: number;
+  yStart: number;
+  xEnd: number;
+  yEnd: number;
+};
 
-const CURSOR_WIDTH = CANVAS_WIDTH / 10;
-const CURSOR_HEIGHT = CANVAS_HEIGHT / 10;
+const showLines = true;
 
-const canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-const cursor = new Cursor(100, 100, 10, 10, CURSOR_WIDTH, CURSOR_HEIGHT);
+const app = document.querySelector<HTMLDivElement>("#app")!;
+let CONTAINER_WIDTH = app.offsetWidth;
+let CONTAINER_HEIGHT = app.offsetHeight;
 
-function cursorFactory(n: number) {
-  const cursorArr = [];
+let CANVAS_WIDTH = CONTAINER_WIDTH;
+let CANVAS_HEIGHT = CONTAINER_HEIGHT;
 
-  // prevent from generating in the wall
-  const maxWidth = CANVAS_WIDTH - CURSOR_WIDTH;
-  const maxHeight = CANVAS_HEIGHT - CURSOR_HEIGHT;
+const canvas = document.querySelector<HTMLCanvasElement>("#canvas")!;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 
-  for (let i = 0; i < n; i++) {
-    cursorArr.push(
-      new Cursor(randomPos(maxWidth), randomPos(maxHeight), 10, 10, 160, 90)
-    );
-  }
+let CURSOR_WIDTH = 160;
+let CURSOR_HEIGHT = 90;
 
-  return cursorArr;
+const ctx = canvas!.getContext("2d")!;
 
-  function randomPos(max: number) {
-    return Math.floor(Math.random() * max);
+const xInp = document.querySelector("#x")! as HTMLInputElement;
+const yInp = document.querySelector("#y")! as HTMLInputElement;
+
+let lines = generateLines(xInp.valueAsNumber, yInp.valueAsNumber);
+let currLine = 0;
+let t = 0;
+
+window.addEventListener("resize", () => {
+  CONTAINER_WIDTH = app.offsetWidth;
+  CONTAINER_HEIGHT = app.offsetHeight;
+
+  CANVAS_WIDTH = CONTAINER_WIDTH;
+  CANVAS_HEIGHT = CONTAINER_HEIGHT;
+
+  canvas.width = CANVAS_WIDTH;
+  canvas.height = CANVAS_HEIGHT;
+
+  lines = generateLines(xInp.valueAsNumber, yInp.valueAsNumber);
+});
+
+run();
+
+function run() {
+  window.requestAnimationFrame(run);
+
+  render(lines);
+
+  t += 0.005 / scaleTime(lines[currLine]);
+
+  if (currLine < lines.length - 1) {
+    if (t > 1) {
+      t = 0;
+      currLine++;
+    }
+  } else if (currLine === lines.length - 1) {
+    if (t > 1) {
+      t = 1;
+    }
   }
 }
 
-// const cursors = cursorFactory(1);
+function scaleTime(line: Line) {
+  const maxLength = Math.sqrt(
+    Math.pow(CANVAS_WIDTH - CURSOR_WIDTH, 2) +
+      Math.pow(CANVAS_HEIGHT - CURSOR_HEIGHT, 2)
+  );
 
-const sim = new Simulation();
-sim.mountCanvas(document.querySelector<HTMLDivElement>("#app")!, canvas);
+  const deltaX = line.xEnd - line.xStart;
+  const deltaY = line.yEnd - line.yStart;
 
-sim.run(canvas, cursor);
+  const lineLength = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
 
-document.querySelector("#start")!.addEventListener("click", () => {
-  sim.toggleRunning();
+  const percentage = lineLength / maxLength;
+  return percentage;
+}
+
+xInp.addEventListener("input", () => {
+  if (xInp.valueAsNumber > CANVAS_WIDTH - CURSOR_WIDTH) {
+    alert(
+      "Please choose a value between 0 and " + (CANVAS_WIDTH - CURSOR_WIDTH)
+    );
+    return;
+  }
+  lines = generateLines(xInp.valueAsNumber, yInp.valueAsNumber);
+  render(lines);
 });
+
+yInp.addEventListener("input", () => {
+  if (yInp.valueAsNumber > CANVAS_HEIGHT - CURSOR_HEIGHT) {
+    alert(
+      "Please choose a value between 0 and " + (CANVAS_HEIGHT - CURSOR_HEIGHT)
+    );
+    return;
+  }
+
+  lines = generateLines(xInp.valueAsNumber, yInp.valueAsNumber);
+  render(lines);
+});
+
+function generateLines(x = 100, y = 100) {
+  const lineArr = [];
+  const line = {
+    xStart: x,
+    yStart: y,
+    xEnd: 0,
+    yEnd: 0,
+  };
+  const v = {
+    x: 10,
+    y: 10,
+  };
+
+  let count = 0;
+
+  while (count < 100) {
+    const dist = {
+      x: 0,
+      y: 0,
+    };
+
+    if (findDirection(v.x, v.y) === "tl") {
+      dist.x = line.xStart;
+      dist.y = line.yStart;
+
+      if (dist.x > dist.y) {
+        v.y = v.y * -1;
+
+        line.xEnd = line.xStart - dist.y;
+        line.yEnd = 0;
+      } else if (dist.x < dist.y) {
+        v.x = v.x * -1;
+
+        line.xEnd = 0;
+        line.yEnd = line.yStart - dist.x;
+      } else if (dist.x === dist.y) {
+        v.x = v.x * -1;
+        v.y = v.y * -1;
+
+        line.xEnd = 0;
+        line.yEnd = 0;
+      }
+
+      lineArr.push({
+        xStart: line.xStart,
+        xEnd: line.xEnd,
+        yStart: line.yStart,
+        yEnd: line.yEnd,
+      });
+
+      line.xStart = line.xEnd;
+      line.yStart = line.yEnd;
+    } else if (findDirection(v.x, v.y) === "tr") {
+      line.xStart += CURSOR_WIDTH;
+
+      dist.x = CANVAS_WIDTH - line.xStart;
+      dist.y = line.yStart;
+
+      if (dist.x > dist.y) {
+        v.y = v.y * -1;
+
+        line.xEnd = line.xStart + dist.y;
+        line.yEnd = 0;
+      } else if (dist.x < dist.y) {
+        v.x = v.x * -1;
+
+        line.xEnd = CANVAS_WIDTH;
+        line.yEnd = line.yStart - dist.x;
+      } else if (dist.x === dist.y) {
+        v.x = v.x * -1;
+        v.y = v.y * -1;
+
+        line.xEnd = CANVAS_WIDTH;
+        line.yEnd = 0;
+      }
+
+      lineArr.push({
+        xStart: line.xStart,
+        xEnd: line.xEnd,
+        yStart: line.yStart,
+        yEnd: line.yEnd,
+      });
+
+      line.xStart = line.xEnd - CURSOR_WIDTH;
+      line.yStart = line.yEnd;
+    } else if (findDirection(v.x, v.y) === "br") {
+      line.xStart += CURSOR_WIDTH;
+      line.yStart += CURSOR_HEIGHT;
+
+      dist.x = CANVAS_WIDTH - line.xStart;
+      dist.y = CANVAS_HEIGHT - line.yStart;
+
+      if (dist.x > dist.y) {
+        v.y = v.y * -1;
+
+        line.xEnd = line.xStart + dist.y;
+        line.yEnd = CANVAS_HEIGHT;
+      } else if (dist.x < dist.y) {
+        v.x = v.x * -1;
+
+        line.xEnd = CANVAS_WIDTH;
+        line.yEnd = line.yStart + dist.x;
+      } else if (dist.x === dist.y) {
+        v.x = v.x * -1;
+        v.y = v.y * -1;
+
+        line.xEnd = CANVAS_WIDTH;
+        line.yEnd = CANVAS_HEIGHT;
+      }
+
+      lineArr.push({
+        xStart: line.xStart,
+        xEnd: line.xEnd,
+        yStart: line.yStart,
+        yEnd: line.yEnd,
+      });
+      line.xStart = line.xEnd - CURSOR_WIDTH;
+      line.yStart = line.yEnd - CURSOR_HEIGHT;
+    } else if (findDirection(v.x, v.y) === "bl") {
+      line.yStart += CURSOR_HEIGHT;
+
+      dist.x = line.xStart;
+      dist.y = CANVAS_HEIGHT - line.yStart;
+
+      if (dist.x > dist.y) {
+        v.y = v.y * -1;
+
+        line.xEnd = line.xStart - dist.y;
+        line.yEnd = CANVAS_HEIGHT;
+      } else if (dist.x < dist.y) {
+        v.x = v.x * -1;
+
+        line.xEnd = 0;
+        line.yEnd = line.yStart + dist.x;
+      } else if (dist.x === dist.y) {
+        v.x = v.x * -1;
+        v.y = v.y * -1;
+
+        line.xEnd = 0;
+        line.yEnd = CANVAS_HEIGHT;
+      }
+
+      lineArr.push({
+        xStart: line.xStart,
+        xEnd: line.xEnd,
+        yStart: line.yStart,
+        yEnd: line.yEnd,
+      });
+      line.xStart = line.xEnd;
+      line.yStart = line.yEnd - CURSOR_HEIGHT;
+    }
+
+    count++;
+  }
+  return lineArr;
+}
+
+function render(lines: Line[]) {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  if (showLines) {
+    ctx.beginPath();
+    ctx.moveTo(lines[currLine].xStart, lines[currLine].yStart);
+    ctx.lineTo(lines[currLine].xEnd, lines[currLine].yEnd);
+    ctx.stroke();
+    // lines.forEach((line) => {
+    //   canvas.getCtx().beginPath();
+    //   canvas.getCtx().moveTo(line.xStart, line.yStart);
+    //   canvas.getCtx().lineTo(line.xEnd, line.yEnd);
+    //   canvas.getCtx().stroke();
+  }
+
+  drawCursorAlongLine(lines[currLine], t);
+}
+
+function drawCursorAlongLine(line: Line, t: number) {
+  const deltaX = line.xEnd - line.xStart;
+  const x = deltaX * t + line.xStart;
+
+  const deltaY = line.yEnd - line.yStart;
+  const y = deltaY * t + line.yStart;
+
+  if (deltaX > 0 && deltaY > 0) {
+    ctx.fillRect(
+      x - CURSOR_WIDTH,
+      y - CURSOR_HEIGHT,
+      CURSOR_WIDTH,
+      CURSOR_HEIGHT
+    );
+  } else if (deltaX > 0 && deltaY < 0) {
+    ctx.fillRect(x - CURSOR_WIDTH, y, CURSOR_WIDTH, CURSOR_HEIGHT);
+  } else if (deltaX < 0 && deltaY > 0) {
+    ctx.fillRect(x, y - CURSOR_HEIGHT, CURSOR_WIDTH, CURSOR_HEIGHT);
+  } else if (deltaX < 0 && deltaY < 0) {
+    ctx.fillRect(x, y, CURSOR_WIDTH, CURSOR_HEIGHT);
+  } else {
+    console.log("error finding edge to draw box from");
+  }
+}
+
+function findDirection(x: number, y: number) {
+  if (x < 0 && y < 0) {
+    return "tl";
+  } else if (x > 0 && y < 0) {
+    return "tr";
+  } else if (x > 0 && y > 0) {
+    return "br";
+  } else if (x < 0 && y > 0) {
+    return "bl";
+  }
+}
+
+// const cursor = new Cursor(100, 100, 10, 10, CURSOR_WIDTH, CURSOR_HEIGHT);
+
+// function cursorFactory(n: number) {
+//   const cursorArr = [];
+
+//   // prevent from generating in the wall
+//   const maxWidth = CANVAS_WIDTH - CURSOR_WIDTH;
+//   const maxHeight = CANVAS_HEIGHT - CURSOR_HEIGHT;
+
+//   for (let i = 0; i < n; i++) {
+//     cursorArr.push(
+//       new Cursor(randomPos(maxWidth), randomPos(maxHeight), 10, 10, 160, 90)
+//     );
+//   }
+
+//   return cursorArr;
+
+//   function randomPos(max: number) {
+//     return Math.floor(Math.random() * max);
+//   }
+// }
+
+// // const cursors = cursorFactory(1);
+
+// const sim = new Simulation();
+// sim.mountCanvas(document.querySelector<HTMLDivElement>("#app")!, canvas);
+
+// sim.run(canvas, cursor);
+
+// document.querySelector("#start")!.addEventListener("click", () => {
+//   sim.toggleRunning();
+// });
